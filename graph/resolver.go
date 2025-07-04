7ce96ee18c6c4ec59b8e9e868fee7c70174b290a
Yi-Jyun Pan <me@pan93.com>
2025-07-04 12:16:50 +0800

    feat(graph): implement Scope directive

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,8 +1,13 @@
 package graph
 
 import (
+	"context"
+	"errors"
+
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/database-playground/backend-v2/ent"
+	"github.com/database-playground/backend-v2/internal/auth"
+	"github.com/database-playground/backend-v2/internal/scope"
 )
 
 // This file will not be regenerated automatically.
@@ -16,5 +21,23 @@ type Resolver struct{ client *ent.Client }
 func NewSchema(client *ent.Client) graphql.ExecutableSchema {
 	return NewExecutableSchema(Config{
 		Resolvers: &Resolver{client},
+		Directives: DirectiveRoot{
+			Scope: func(ctx context.Context, obj any, next graphql.Resolver, fnScope string) (res any, err error) {
+				if fnScope == "" {
+					return next(ctx)
+				}
+
+				user, ok := auth.GetUser(ctx)
+				if !ok {
+					return nil, errors.New("unauthorized")
+				}
+
+				if scope.ShouldAllow(fnScope, user.Scopes) {
+					return next(ctx)
+				}
+
+				return nil, errors.New("no sufficient scope")
+			},
+		},
 	})
 }
