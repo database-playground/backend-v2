a0ec241c34e840e43382b2886e134c09b2157f7a
Yi-Jyun Pan <me@pan93.com>
2025-07-06 23:11:27 +0800

    refactor(graph): improve error code

--- graph/defs/errors.go
+++ graph/defs/errors.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// GqlError is the extension of an error with a code.
 type GqlError struct {
 	Message string
 	Code    string
@@ -13,30 +14,37 @@ func (e GqlError) Error() string {
 	return fmt.Sprintf("%s: %s", e.Code, e.Message)
 }
 
+// NewErrNoSufficientScope creates a "no sufficient scope" error.
 func NewErrNoSufficientScope(requireScope string) GqlError {
 	return GqlError{
 		Message: fmt.Sprintf("no sufficient scope: %s", requireScope),
-		Code:    CodeForbidden,
+		Code:    CodeUnauthorized,
 	}
 }
 
+// ErrNotFound is the error for "not found".
 var ErrNotFound = GqlError{
 	Message: "not found",
 	Code:    CodeNotFound,
 }
 
+// ErrUnauthorized is the error for "require authentication".
 var ErrUnauthorized = GqlError{
 	Message: "require authentication",
 	Code:    CodeUnauthorized,
 }
 
+// ErrVerified is the error for "user already verified".
 var ErrVerified = GqlError{
 	Message: "user already verified",
-	Code:    "USER_VERIFIED",
+	Code:    CodeUserVerified,
 }
 
 const (
-	CodeNotFound     = "NOT_FOUND"
+	// CodeNotFound is the error code for "not found".
+	CodeNotFound = "NOT_FOUND"
+	// CodeUnauthorized is the error code for "require authentication".
 	CodeUnauthorized = "UNAUTHORIZED"
-	CodeForbidden    = "FORBIDDEN"
+	// CodeUserVerified is the error code for "user already verified".
+	CodeUserVerified = "USER_VERIFIED"
 )
