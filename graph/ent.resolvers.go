e84189b1f26df799ef7f6fc8b8f0f4140c58ebc1
Yi-Jyun Pan <me@pan93.com>
2025-07-06 16:49:08 +0800

    refactor(graph): move user-specific methods to user.graphqls

--- graph/ent.resolvers.go
+++ graph/ent.resolvers.go
@@ -42,7 +42,5 @@ func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 // User returns UserResolver implementation.
 func (r *Resolver) User() UserResolver { return &userResolver{r} }
 
-type (
-	queryResolver struct{ *Resolver }
-	userResolver  struct{ *Resolver }
-)
+type queryResolver struct{ *Resolver }
+type userResolver struct{ *Resolver }
