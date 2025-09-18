473453d7b8ab42ca52d38ffbee175d3c87427cea
Yi-Jyun Pan <me@pan93.com>
2025-09-18 21:31:55 +0800

    refactor(submission): rewrite SubmitAnswer

--- internal/submission/submission.go
+++ internal/submission/submission.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"reflect"
 
 	"github.com/database-playground/backend-v2/ent"
+	"github.com/database-playground/backend-v2/ent/submission"
 	"github.com/database-playground/backend-v2/internal/events"
 	"github.com/database-playground/backend-v2/internal/sqlrunner"
 	"github.com/database-playground/backend-v2/models"
-	"github.com/samber/lo"
 )
 
 type SubmissionService struct {
@@ -30,40 +31,43 @@ type SubmitAnswerInput struct {
 
 var ErrQuestionNotFound = errors.New("question not found")
 
-func (ss *SubmissionService) SubmitAnswer(ctx context.Context, input SubmitAnswerInput) (models.SubmissionResult, error) {
+// SubmitAnswer submits an answer from a user to a question.
+func (ss *SubmissionService) SubmitAnswer(ctx context.Context, input SubmitAnswerInput) (*ent.Submission, error) {
 	question, err := ss.entClient.Question.Get(ctx, input.QuestionID)
 	if err != nil {
 		if ent.IsNotFound(err) {
-			return models.SubmissionResult{}, ErrQuestionNotFound
+			return nil, ErrQuestionNotFound
 		}
 
-		return models.SubmissionResult{}, fmt.Errorf("get question: %w", err)
+		return nil, fmt.Errorf("get question: %w", err)
 	}
 
 	database, err := question.QueryDatabase().Only(ctx)
 	if err != nil {
-		return models.SubmissionResult{}, fmt.Errorf("get database: %w", err)
+		return nil, fmt.Errorf("get database: %w", err)
 	}
 
-	submissionResult := models.SubmissionResult{
-		Version: "1",
-	}
-
-	response, err := ss.sqlrunner.Query(ctx, database.Schema, input.Answer)
-	if err != nil {
-		submissionResult.Error = lo.ToPtr(err.Error())
-	} else {
-		submissionResult.Result = &response
-	}
-
-	// Write submission to database
-	submission, err := ss.entClient.Submission.Create().
+	submissionModel := ss.entClient.Submission.Create().
 		SetQuestion(question).
-		SetResult(submissionResult).
-		SetUserID(input.SubmitterID).
-		Save(ctx)
+		SetUserID(input.SubmitterID)
+
+	result, err := ss.runAnswer(ctx, database.Schema, input.Answer, question.ReferenceAnswer)
 	if err != nil {
-		return models.SubmissionResult{}, fmt.Errorf("create submission: %w", err)
+		submissionModel.SetError(err.Error())
+		submissionModel.SetStatus(submission.StatusFailed)
+	} else {
+		submissionModel.SetQueryResult(result)
+
+		if result.MatchAnswer {
+			submissionModel.SetStatus(submission.StatusSuccess)
+		} else {
+			submissionModel.SetStatus(submission.StatusFailed)
+		}
+	}
+
+	submission, err := submissionModel.Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("create submission: %w", err)
 	}
 
 	// Write event to database
@@ -76,5 +80,34 @@ func (ss *SubmissionService) SubmitAnswer(ctx context.Context, input SubmitAnswe
 		UserID: input.SubmitterID,
 	})
 
-	return submissionResult, nil
+	return submission, nil
+}
+
+// runAnswer runs both the reference answer and the users' answer, compare them,
+// and return the result of this submission.
+func (ss *SubmissionService) runAnswer(ctx context.Context, schema, answer, referenceAnswer string) (*models.UserSQLExecutionResult, error) {
+	// run the reference answer
+	referenceAnswerResponse, err := ss.sqlrunner.Query(ctx, schema, referenceAnswer)
+	if err != nil {
+		return nil, fmt.Errorf("cannot run reference answer: %w", err)
+	}
+
+	// run the user's answer
+	response, err := ss.sqlrunner.Query(ctx, schema, answer)
+	if err != nil {
+		return nil, fmt.Errorf("cannot run user's answer: %w", err)
+	}
+
+	return &models.UserSQLExecutionResult{
+		SQLExecutionResult: models.SQLExecutionResult{
+			Columns: response.Columns,
+			Rows:    response.Rows,
+		},
+		MatchAnswer: CompareAnswer(response, referenceAnswerResponse),
+	}, nil
+}
+
+// CompareAnswer compares the answer to the reference answer.
+func CompareAnswer(answer sqlrunner.DataResponse, referenceAnswer sqlrunner.DataResponse) bool {
+	return reflect.DeepEqual(answer, referenceAnswer)
 }
